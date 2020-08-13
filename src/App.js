aa70b173edb661f32f5a25db38386ee89a1c3b09
Antonio <merendaz@gmail.com>
2020-08-13 10:06:03 -0400

    Styled the icon and the images, wrapped the images in IconButtons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import Header from "./Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the Tinder Clone App!</h1>
       {/* Header */}
       <Header />
       {/* Tinder Cards */}
