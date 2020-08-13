e088f3ea45307a73a31a97e9e9ddd54e9d3565b4
Antonio <merendaz@gmail.com>
2020-08-13 11:08:07 -0400

    Added the Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
       <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <h1>I am Chatpage</h1>
+          </Route>
+          <Route path="/">
+            <h1>I am Homepage</h1>
+          </Route>
+        </Switch>
+      </Router>
+      {/* Header */}
       {/* Tinder Cards */}
       {/* Buttons below tinder cards */}
 
